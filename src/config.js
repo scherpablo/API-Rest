f278c92b5dcbbcb952ab94187dca8687407b7660
scherpablo <scherpablo@gmail.com>
2023-05-11 22:03:47 -0300

    first commit

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ config();
 export const PORT = process.env.PORT || 3000;
 
 export const MYSQLHOST = process.env.HOST || 'localhost';
-export const MYSQLUSER = process.env.USER || 'root';
-export const MYSQLPASSWORD = process.env.PASSWORD || '';
 export const MYSQLDATABASE = process.env.DATABASE || 'estudio_scherpa';
+export const MYSQLPASSWORD = process.env.PASSWORD || '';
 export const MYSQLPORT = process.env.DBPORT || 3306;
+export const MYSQLUSER = process.env.USER || 'root';
