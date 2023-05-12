ed27b9928b7de6b92335af96bca4c1355b7c5827
scherpablo <scherpablo@gmail.com>
2023-05-11 22:30:12 -0300

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 //Importamos createPool from mysql2
 import { createPool }  from "mysql2/promise";
 //Importamos las variables de entorno
-import { MYSQLHOST, MYSQLDATABASE, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER } from "./config.js";
+import { MYSQLHOST, MYSQLDATABASE, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER } from "../config.js";
 
 export const POOL = createPool ({
-    host: "localhost",  
-    user: "root",
-    password: "",
-    database: "estudio_scherpa",
-    port: 3306
+    host: MYSQLHOST,
+    database: MYSQLDATABASE,  
+    password: MYSQLPASSWORD,
+    port: MYSQLPORT,
+    user: MYSQLUSER
 })
 
